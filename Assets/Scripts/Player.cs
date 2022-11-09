055099e7afdcee0ea77c5ac4fc6729f4d123daba
RayKST <jailma.kmp@gmail.com>
2022-11-09 20:12:10 -0300

    [feat] new method to move the main camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,21 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-   
+
+    }
+
+    void CameraMovement()
+    {
+        var lookAtPos = Input.mousePosition;
+        lookAtPos.z = transform.position.z - Camera.main.transform.position.z;
+        lookAtPos = Camera.main.ScreenToWorldPoint(lookAtPos);
+        transform.up = lookAtPos - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float playerVelocity = 30.0f;
+        float playerVelocity = 10.0f;
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, 0.0f, verticalInput);
