795c6e6b343d262a049e52a573d7dad5d778bfa2
Gabriel Rodrigues <gabrielsousa455@gmail.com>
2022-11-09 20:25:04 -0300

    transition of animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-   
+        animator = GetComponent <Animator>();
     }
 
     // Update is called once per frame
@@ -18,5 +21,16 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, 0.0f, verticalInput);
         transform.Translate(direction * playerVelocity * Time.deltaTime);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            print("Estou andando");
+            animator.SetBool("isMoving", true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            print("Parei de andar");
+            animator.SetBool("isMoving", false);
+        }
     }
 }
